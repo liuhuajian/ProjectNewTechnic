657a044420be4136e06d8703b0835d48de7335a0
liuhuajian <liuhuajian_lhj@163.com>
2017-08-29 00:02:00 +0800

    新增二维码扫描

--- app/src/main/java/messi/lhj/com/projectnewtechnic/MyApplication.java
+++ app/src/main/java/messi/lhj/com/projectnewtechnic/MyApplication.java
@@ -3,6 +3,7 @@ package messi.lhj.com.projectnewtechnic;
 import android.app.Application;
 
 import com.tencent.bugly.crashreport.CrashReport;
+import com.uuzuche.lib_zxing.activity.ZXingLibrary;
 
 /**
  * Created by messi on 2017/8/27.
@@ -13,6 +14,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        CrashReport.initCrashReport(getApplicationContext(),"6c9ca605a4",true);
+        CrashReport.initCrashReport(getApplicationContext(),"6c9ca605a4",false);
+        ZXingLibrary.initDisplayOpinion(getApplicationContext());
     }
 }
---
+++ app/src/main/java/messi/lhj/com/projectnewtechnic/TestActivity.java
@@ -0,0 +1,69 @@
+package messi.lhj.com.projectnewtechnic;
+
+import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.uuzuche.lib_zxing.activity.CaptureActivity;
+import com.uuzuche.lib_zxing.activity.CodeUtils;
+
+import messi.lhj.com.projectnewtechnic.util.CheckPermissionUtils;
+import messi.lhj.com.projectnewtechnic.util.Logger;
+
+public class TestActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bugly_test);
+        initPermission();
+    }
+
+    public void btnOnclick(View view) {
+        new CommonDialog(this,R.style.dialog);
+    }
+
+    public void ZxBtnOnclick(View view) {
+        Intent intent = new Intent(TestActivity.this, CaptureActivity.class);
+        startActivityForResult(intent,REQUEST_CODE);
+    }
+
+    private void initPermission() {
+        //检查权限
+        String[] permissions = CheckPermissionUtils.checkPermission(this);
+        if (permissions.length == 0) {
+            //权限都申请了
+            //是否登录
+        } else {
+            //申请权限
+            ActivityCompat.requestPermissions(this, permissions, 100);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Logger.d("onActivityResult");
+        if (requestCode ==REQUEST_CODE){
+            Logger.d("onActivityResult");
+            if (data==null)
+                return;
+            Bundle bundle = data.getExtras();
+            if (bundle == null) {
+                return;
+            }
+            Logger.d("onActivityResult");
+            if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_SUCCESS) {
+                String result = bundle.getString(CodeUtils.RESULT_STRING);
+                Toast.makeText(this, "解析结果:" + result, Toast.LENGTH_LONG).show();
+            } else if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_FAILED) {
+                Toast.makeText(TestActivity.this, "解析二维码失败", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
